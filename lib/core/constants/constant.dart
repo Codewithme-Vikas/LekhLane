839b872e02fd51f6ffd2d56ce37fbddea9478002
vikash <vikashnagar2025@gmail.com>
2026-04-18 22:08:36 +0530

    feat : endpoints to connect the backend ( backend is not connected yet ) msg : baseUrl can seems to be weird now as backend is running on pc and app on mobile, so connected by wifi

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -1,5 +1,45 @@
 class Constant {
+  //static const baseUrl = "http://127.0.0.1:3000";
+  static const String baseUrl = 'http://192.168.1.48:3000/api/v1';
+}
 
-  static const backendUrl = "http://127.0.0.1:3000";
+class EndPoint {}
 
+class UserEndPoint extends EndPoint {
+  static const sendOtp = "${Constant.baseUrl}/sendOTP"; // post
+  static const signUp = "${Constant.baseUrl}/signup"; // post
+  static const login = "${Constant.baseUrl}/login"; // post
+  static const forgetPassword = "${Constant.baseUrl}/password/forget"; // post
+  static const resetPassword =
+      "${Constant.baseUrl}/password/reset/:token"; // put
+  static const updatePassword = "${Constant.baseUrl}/password/update"; // put
+  static const getUserDetials = "${Constant.baseUrl}/user/:id"; // get
+  static const updateProfile = "${Constant.baseUrl}/me/update"; // put
+}
+
+class BlogEndPoint extends EndPoint {
+  static const create = "${Constant.baseUrl}/post/create"; // post
+  static const getAll = "${Constant.baseUrl}/post/all"; // get
+  static const getDetail = "${Constant.baseUrl}/post/:id"; // get
+  static const update = "${Constant.baseUrl}/post/:id"; // put
+  static const delete = "${Constant.baseUrl}/post/:id"; // delete
+  static const upVote = "${Constant.baseUrl}/post/:id/upvote"; // get
+  static const downVote = "${Constant.baseUrl}/post/:id/downvote"; // get
+
+  // Admin
+  static const deleteByAdmin = "${Constant.baseUrl}/admin/post/:id"; // delete
+}
+
+class CommentEndPoint extends EndPoint {
+  static const create = "${Constant.baseUrl}/post/comment/create"; // post
+  static const getBlogAllComments =
+      "${Constant.baseUrl}/post/:postId/comment/all"; // get
+  static const update = "${Constant.baseUrl}/post/comment/:id"; // put
+  static const delete = "${Constant.baseUrl}/post/comment/:id"; // delete
+  static const upVote = "${Constant.baseUrl}/post/comment/:id/upvote"; // get
+  static const downVote = "${Constant.baseUrl}/post/comment/:downvote"; // get
+
+  // Admin
+  static const deleteByAdmin =
+      "${Constant.baseUrl}/admin/post/comment/:id"; // delete
 }
--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,16 +1,19 @@
 import 'package:blog_app/core/constants/constant.dart';
 import 'package:dio/dio.dart';
 
-
 class DioClient {
   final Dio dio;
 
   DioClient()
     : dio = Dio(
-      BaseOptions(baseUrl: Constant.backendUrl,connectTimeout: Duration(seconds: 12),receiveTimeout: Duration(seconds: 13),
-      headers: {
-        "Content-Type" : "application/json",
-        "Accept" : "application/json"
-      })
-    );
+        BaseOptions(
+          baseUrl: Constant.baseUrl,
+          connectTimeout: Duration(seconds: 12),
+          receiveTimeout: Duration(seconds: 13),
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        ),
+      );
 }
